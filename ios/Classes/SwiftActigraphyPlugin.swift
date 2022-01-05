9811be9cf05d42d4f49325cf61e705cc1c97d48e
Yaoyu Chen <yaoyuchen@eml.cc>
2022-01-04 23:19:18 -0800

    Implement Core Motion for iOS.

--- ios/Classes/SwiftActigraphyPlugin.swift
+++ ios/Classes/SwiftActigraphyPlugin.swift
@@ -1,3 +1,5 @@
+// WARNING: Not tested on real device yet.
+
 import Flutter
 import UIKit
 
@@ -9,6 +11,97 @@ public class SwiftActigraphyPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    /// Handle checkIfActigraphyIsAvailable
+    if (call.method.elementsEqual("checkIfActigraphyIsAvailable")) {
+      checkIfActigraphyIsAvailable(result: result)
+    }
+
+    /// Handle checkAuthorizationStatus
+    if (call.method.elementsEqual("checkAuthorizationStatus")) {
+      checkAuthorizationStatus(result: result)
+    }
+
+    /// Handle getActigraphyData
+    if (call.method.elementsEqual("getActigraphyData")) {
+      getActiographyData(call:call, result: result)
+    }
+  }
+
+  func getActiographyData(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let args = call.arguments as! [String: Any]
+    let fromDaysAgo = args["fromDaysAgo"] as! Int
+
+    let now = Date()
+    let startDate = now.addingTimeInterval(-TimeInterval(fromDaysAgo * 24 * 60 * 60))
+
+    let activityData: String = "activityStartDate,activityType,confidence\n"
+    
+    let activityManager = CMMotionActivityManager()
+    activityManager.queryActivityStarting(from:startDate, to: now, to: OperationQueue.init()) {
+        (activities, error) in
+        if error != nil {
+            activityData = "Error: \(error!.localizedDescription)"
+        } else {
+            for activity in activities! {
+                let startDate = activity.startDate
+                let type = determineActivityType(activity: activity)
+                let confidence = standardizeConfidence(activity: activity.confidence)
+                activityData += "\(startDate),\(type),\(confidence)\n"
+            }
+        }
+    }
+    result(activityData)
+  }
+
+  func determineActivityType(activity: CMMotionActivity) -> String {
+    // Activity types are not mutually exclusive, so we have to check for all possible cases.
+    // Return 'stationary' only if the activity is exclusively stationary.
+    if activity.unknown {
+        return "unknown"
+    } else if activity.walking {
+        return "walking"
+    } else if activity.running {
+        return "running"
+    } else if activity.automotive {
+        return "automotive"
+    } else if activity.cycling {
+        return "cycling"
+    } else if activity.stationary {
+        return "stationary"
+    } else {
+        return "NULL"
+    }
+  }
+
+  func standardizeConfidence(activity: CMMotionActivityConfidence) -> String {
+    switch activity {
+    case .high:
+        return "3"
+    case .medium:
+        return "2"
+    case .low:
+        return "1"
+    default:
+        return "NULL"
+    }
+  }
+  
+  func checkIfActigraphyIsAvailable(result: @escaping FlutterResult) {
+    let activityManager = CMMotionActivityManager()
+    if activityManager.isActivityAvailable {
+        result(true)
+    } else {
+        result(false)
+    }
+  }
+
+  func checkAuthorizationStatus(result: @escaping FlutterResult) {
+    let activityManager = CMMotionActivityManager()
+    let statusRaw = activityManager.authorizationStatus()
+    if statusRaw == .authorized {
+        result(true)
+    } else {
+        result(false)
+    }
   }
 }
--- lib/actigraphy.dart
+++ lib/actigraphy.dart
@@ -6,8 +6,35 @@ import 'package:flutter/services.dart';
 class Actigraphy {
   static const MethodChannel _channel = MethodChannel('actigraphy');
 
+  /// platformVersion
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  /// getActigraphyData(int fromDaysAgo)
+  /// fromDaysAgo: number of days ago to start the query
+  /// returns: a csv string of the data
+  /// 
+  /// TODO: return a list of ActigraphyData objects instead
+  static Future<String?> getActigraphyData(int fromDaysAgo) async {
+    final String? data = await _channel.invokeMethod('getActigraphyData', <String, dynamic>{
+      'fromDaysAgo': fromDaysAgo,
+    });
+    return data;
+  }
+
+  /// isAvailable
+  /// returns: true if the device can provide data
+  static Future<bool> get isAvailable async {
+    final bool available = await _channel.invokeMethod('checkIfActigraphyIsAvailable');
+    return available;
+  }
+
+  /// isAuthorized
+  /// returns: true if the user has authorized the app to access data
+  static Future<bool> get isAuthorized async {
+    final bool authorized = await _channel.invokeMethod('checkAuthorizationStatus');
+    return authorized;
   }
 }
